35b9339e75b4f0c8b0d26419a7c647fdc8af6848
René Ruppert <rene.ruppert@gmail.com>
2020-06-01 19:06:12 +0200

    Add Blazorise

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 namespace DashboardClient
 {
@@ -15,11 +18,31 @@ namespace DashboardClient
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
 
+            builder.Services
+              .AddBlazorise(options =>
+             {
+                  options.ChangeTextOnKeyPress = true;
+              })
+              .AddBootstrapProviders()
+              .AddFontAwesomeIcons();
+
+            builder.Services.AddSingleton(new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
+
+            builder.RootComponents.Add<App>("app");
+
+            var host = builder.Build();
+
+            host.Services
+              .UseBootstrapProviders()
+              .UseFontAwesomeIcons();
+
+            await host.RunAsync();
         }
     }
 }
